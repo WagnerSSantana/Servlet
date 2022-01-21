c611d3db7e4e7b30dc58b0da8d354116afd632ac
WagnerSSantana <50252454+WagnerSSantana@users.noreply.github.com>
2022-01-21 17:23:14 -0300

    Form Empresa , 3 modulo

--- src/main/java/br/com/softplan/gerenciador/servelt/OiMundo.java
+++ src/main/java/br/com/softplan/gerenciador/servelt/OiMundo.java
@@ -3,7 +3,7 @@ package br.com.softplan.gerenciador.servelt;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,8 +13,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = "/oi")
 public class OiMundo  extends HttpServlet{
 	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 	
 		PrintWriter out = resp.getWriter();
 		out.println ("<html");
